05408ce16480a3db93f0e5dc2f6b9540f69b0c78
Alexia Peresson <alexiaperesson@MacBook-Pro-de-Alexia.local>
2018-06-03 22:05:52 +0200

    create repo

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-'use strict';
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express().use(bodyParser.json()); // creates express http server
-
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+console.log("hello world");
